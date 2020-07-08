842a752c0690207a4b696721ca66d2d03d5f8297
Daria Kozlova <nargle30@gmail.com>
2020-07-08 17:24:10 +0200

    sentry setup

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+    dsn: 'xxxx',
+});
+
 
 AppRegistry.registerComponent(appName, () => App);
